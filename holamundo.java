668db4d04f0adc0badf6adcff9d7c9a3c970d65c
Jhoely Martinez Cruz <jhoelymartinezcruz@gmail.com>
2018-04-26 16:43:55 -0400

    se añadio el archivo holamundo.java

---
+++ holamundo.java
@@ -0,0 +1,14 @@
+/**
+*ejemplo de java, mostrando un mensaje: "hola mundo"
+*/
+
+public class holamundo {
+/**
+*el public static sirve para 
+*/
+public static void main (String [] args){
+System.out.println ("¡Hola Mundo!");
+}
+}
+
+
